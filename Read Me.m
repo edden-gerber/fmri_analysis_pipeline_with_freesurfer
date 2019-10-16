5b13266fd6fbe6e43d3711d3dd404a5c313e475f
Edden M. Gerber <edden.gerber@gmail.com>
2019-10-16 12:29:30 +0300

    Update and rename Read Me.m to Read Me.md

--- Read Me.m
+++
@@ -1,3 +0,0 @@
-These are scripts and functions written by Edden Gerber to analyze fMRI data using FreeSurfer and its functional analysis library FSFast. 
-See the white paper "How to analyze fMRI data with FreeSurfer and FSFAST" for a description of this pipeline. 
-
